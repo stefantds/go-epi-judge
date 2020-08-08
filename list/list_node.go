e3e6f67c251de791831885c4077a1bf9844fb05c
Stefan Tudose <stefan.tudose@data4life.care>
2020-08-08 11:29:22 +0200

    list string method to support cycles

--- list/list_node.go
+++ list/list_node.go
@@ -12,9 +12,20 @@ type ListNode struct {
 
 func (l ListNode) String() string {
 	var buf bytes.Buffer
+	visited := make(map[*ListNode]bool)
+
 	for current := &l; current != nil; current = current.Next {
+		_, seen := visited[current]
+
+		if seen {
+			fmt.Fprintf(&buf, "(cycle to %v)", current.Data)
+			return buf.String()
+		}
+
+		visited[current] = true
 		fmt.Fprintf(&buf, "%v -> ", current.Data)
 	}
+
 	fmt.Fprintf(&buf, "%v", nil)
 	return buf.String()
 }

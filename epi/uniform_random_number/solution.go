5c4d478e5620b2b0e7d4c4f5090142cc55814597
Stefan Tudose <stefan.tudose@data4life.care>
2021-04-04 19:42:14 +0200

    add provided function for uniform random

--- epi/uniform_random_number/solution.go
+++ epi/uniform_random_number/solution.go
@@ -1,5 +1,11 @@
 package uniform_random_number
 
+import "math/rand"
+
+func zeroOneRandom() int {
+	return rand.Intn(2)
+}
+
 func UniformRandom(lowerBound int, upperBound int) int {
 	// TODO - Add your code here
 	return 0
